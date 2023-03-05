517e54195643d32ce3e137e36f3216f03aa6349b
Swapnil <swapniljare9601@gmail.com>
2023-03-05 14:05:15 +0530

    Solution 1

--- src/main/java/com/driver/User.java
+++ src/main/java/com/driver/User.java
@@ -3,4 +3,16 @@ package com.driver;
 public class User {
     private String name;
     private String mobile;
+
+    public User(String name, String mobile) {
+        this.name = name;
+        this.mobile = mobile;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public String getMobile(){
+        return mobile;
+    }
 }
